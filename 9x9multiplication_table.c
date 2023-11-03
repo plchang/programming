c3473e3e10b2e0e117df9734efd3aece36854d91
plchang <149220351+plchang@users.noreply.github.com>
2023-11-03 19:11:16 +0800

    Update 9x9multiplication_table.c

--- 9x9multiplication_table.c
+++ 9x9multiplication_table.c
@@ -3,36 +3,20 @@
 
 int main()
 {
-    for (int i = 1;i < 10;i ++){
-        for(int j = 1;j < 4;j ++){
-        
-            printf("%d X %d = %d    ", j, i, i * j);
-
-            if(i * j < 10) printf(" ");
-        }
-
-        printf("\n");
-    }
-
-    printf("\n");
-    for (int i = 1;i < 10;i ++){
-        for(int j = 4;j < 7;j ++){
-            printf("%d X %d = %d    ", j, i, i * j);
-            if(i * j < 10) printf(" ");
-        }
-
-        printf("\n");
-    }
-
-    printf("\n");
-    for (int i = 1;i < 10;i ++){
-        for(int j = 7;j < 10;j ++){
+    for(int k = 1;k < 10;k = k + 3){
+        for (int i = 1;i < 10;i ++){
+            for(int j = k ;j < 3 + k;j ++){
             
-            printf("%d X %d = %d    ", j, i, i * j);
-            if(i * j < 10) printf(" ");
+                printf("%d X %d = %d    ", j, i, i * j);
 
+                if(i * j < 10) printf(" ");
+            }
+
+            printf("\n");
         }
 
         printf("\n");
+
     }
+   
 }
